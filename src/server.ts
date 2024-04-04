a658aed08ff3a36dfaa6ac7bac00aad521846c68
MarianaMendanha <mariana.mendanha.mm@gmail.com>
2024-04-04 16:15:22 -0300

    feat: database migrations and new schema, lib, utils, packages and tests with api

--- src/server.ts
+++ src/server.ts
@@ -1,47 +1,20 @@
 import fastify from "fastify";
-import { z } from 'zod';
-import { PrismaClient } from '@prisma/client';
+import { serializerCompiler, validatorCompiler }  from "fastify-type-provider-zod";
+import { createEvent } from "./routes/create-event";
+import { registerForEvent } from "./routes/register-for-event";
+import { getEvent } from "./routes/get-event";
+import { getAttendeeBadge } from "./routes/get-attendee-badge";
+
 
 const app = fastify()
 
-// conexao com banco de dados
-const prisma = new PrismaClient({
-    log: ['query'],
-})
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
 
-// // Rota da home
-// app.get('/', () => {
-//     return 'Hellou da Mari'
-// })
-
-// app.get('/teste', () => {
-//     return 'Teste'
-// })
-
-app.post('/events', async (request, reply) => {
-    console.log(request.body)
-    const createEventSchema = z.object({
-        title: z.string().min(4),
-        details: z.string().nullable(),
-        slug: z.string(),
-        maximumAttendees: z.number().int().positive().nullable(),
-    })
-
-    // validação com o molde usando o parse
-    const data = createEventSchema.parse(request.body)
-
-    const event = await prisma.event.create({
-        data: {
-            title: data.title,
-            details: data.details,
-            slug: data.slug,
-            maximumAttendees: data.maximumAttendees
-        },
-    })
-
-    // return { eventId : event.id }
-    return reply.status(201).send({ eventId : event.id })
-})
+app.register(createEvent)
+app.register(registerForEvent)
+app.register(getEvent)
+app.register(getAttendeeBadge)
 
 // Servidor na porta 3333
 app.listen({port: 3333}).then(() => {
